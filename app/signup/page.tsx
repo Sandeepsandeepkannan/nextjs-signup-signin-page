499d5e8e8e132073bddd88959a0c9df772b84c55
Sandeep <sandeepsandeepkannan16@gmail.com>
2025-12-16 23:54:48 +0530

    added connection between signup page submission to backend

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,13 +1,34 @@
+"use client";
 
+import { useRef } from "react"
 
 export default function Signuppage(){
+
+       const nameref=useRef<HTMLInputElement>(null)
+       const passwordref=useRef<HTMLInputElement>(null)
+    
+    
+      async function Sendsignupdata(){
+        console.log("data is sending from frontend")
+        
+        const name=nameref.current?.value
+        const password=passwordref.current?.value
+
+        console.log(name,password)
+         
+         await fetch("http://localhost:3000/api/users",{method:"POST",headers:{  "Content-Type": "application/json"}, body: JSON.stringify({ name, password })})
+
+         console.log("Data sended to backend")
+
+      }
+
     return <div className="bg-white h-screen flex  items-center justify-center "> 
               <div className="bg-gray-600 rounded-2xl h-100 w-120 ">
 
-                  <input className="bg-white h-12 w-80 rounded m-17 pl-28 text-2xl text-blue-500 placeholder:text-gray-400" type="text" placeholder="Name" />
-                  <input className="bg-white h-12 w-80 rounded ml-17 pl-28 text-2xl text-blue-500 placeholder:text-gray-400" type="text" placeholder="Password" />
+                  <input ref={nameref} className="bg-white h-12 w-80 rounded m-17 pl-28 text-2xl text-blue-500 placeholder:text-gray-400" type="text" placeholder="Name" />
+                  <input ref={passwordref} className="bg-white h-12 w-80 rounded ml-17 pl-28 text-2xl text-blue-500 placeholder:text-gray-400" type="text" placeholder="Password" />
                     <div  className="ml-35 mt-10">
-                    <button className="bg-blue-700 rounded h-10 w-40 "> Sign Up</button>
+                    <button onClick={Sendsignupdata} className="bg-blue-700 rounded h-10 w-40 "> Sign Up</button>
                     </div>
               </div>
           </div>
